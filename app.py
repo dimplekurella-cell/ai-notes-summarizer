a24798921ae93163f87f5c3f16116da7f1083193
dimplekurella <dimplekurella@gmail.com>
2026-06-10 18:35:56 +0530

    added pdf summarization feature

--- app.py
+++ app.py
@@ -1,5 +1,19 @@
 import streamlit as st
+import nltk
 
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt')
+
+try:
+    nltk.data.find('tokenizers/punkt_tab')
+except LookupError:
+    nltk.download('punkt_tab')
+from PyPDF2 import PdfReader
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
 st.set_page_config(page_title="AI Notes Summarizer", page_icon="📝")
 
 st.title("📝 AI Notes Summarizer")
@@ -10,11 +24,22 @@ uploaded_file = st.file_uploader("Upload a PDF file", type=["pdf"])
 text_input = st.text_area("Or paste your text here")
 
 if st.button("Summarize"):
-    if uploaded_file:
-        st.success("PDF uploaded successfully!")
-        st.write("Summarization logic will be added here.")
-    elif text_input:
-        st.success("Text received!")
-        st.write("Summarization logic will be added here.")
-    else:
-        st.warning("Please upload a PDF or enter some text.")
+
+
+    pdf_reader = PdfReader(uploaded_file)
+    text = ""
+
+    for page in pdf_reader.pages:
+        extracted = page.extract_text()
+        if extracted:
+            text += extracted
+
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LsaSummarizer()
+
+    summary = summarizer(parser.document, 5)
+
+    st.subheader("Summary")
+    for sentence in summary:
+        st.write(sentence)
+    
